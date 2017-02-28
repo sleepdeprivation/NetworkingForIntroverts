274f298920746d14f2e9cc5b139ed411deaed8a2
Ubuntu <ubuntu@ip-172-31-12-63.us-west-1.compute.internal>
2017-02-28 00:55:46 +0000

    Worked out the spidering sytem, about to do graph analysis

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ import json
 from collections import Counter
 import pprint as pr
 from pymongo import MongoClient
+import networkx as nx
+from sets import Set
 import thread
+import time
+import dill
+from matplotlib import pyplot as plt
 
 #Variables that contains the user credentials to access Twitter API 
 access_token = "405233890-AyOaC3V3jdIFIWmbsFV7qTflNumAUvBDGHPnh682"
@@ -24,13 +29,91 @@ class FollowSpider():
 	""""""
 	def __init__(self, API):
 		self.api = API;
+		self.G=nx.DiGraph();
+		self.seen = Set();
+		self.counter = Counter();
 		
-	def recieveStream(self):
-		pass
+	def getFollowListFirstPage(self, identifier):
+		"""Just want the first page of following, to avoid getting trapped"""
+		cur = tweepy.Cursor(self.api.friends_ids, id=identifier).pages(1);#.items()
+		return [i for i in cur];
 		
-	def getFollowList(self):
-		for status in tweepy.Cursor(self.api.user_timeline).items():
-			pr.pprint(status._json);
+	def addToGraph(self, identifier):
+		"""
+			Have we seen this user before?
+			If not, go through it's followers and add them to the count
+		"""
+		if(identifier not in self.seen):
+			self.seen.add(identifier);
+			self.G.add_node(identifier);
+			for page in self.getFollowListFirstPage(identifier):
+				for i in page:
+					print(identifier, i);
+					self.counter[identifier] += 1;
+					self.G.add_node(i);
+					self.G.add_edge(identifier, i);
+			return True;
+		else:
+			return False;
+				
+	def processNextUser(self):
+		"""
+			go through the most common users and add
+			everyone they follow to the graph
+			keep trying until we find one that hasn't been seen
+		"""
+		for i in self.counter.most_common():
+			if(self.addToGraph(str(i[0]))):
+				#graph is too big :(
+				#nx.drawing.nx_pydot.write_dot(self.G, "g.dot")
+				#plt.figure();
+				#nx.draw(self.G);
+				#print("plotting...");
+				#plt.savefig("graph.png")#(block=False);
+				break;
+			else:
+				continue;
+		print("Completed");
+		
+	def startSpider(self, counter):
+		"""
+			Loop forever adding more nodes to graph
+		"""
+		self.counter = counter;
+		while(True):
+			self.processNextUser();
+			self.saveSpider();
+			print("sleeping...");
+			time.sleep(60);
+			
+	def resumeSpider(self):
+		print("called resume");
+		time.sleep(2);
+		self.startSpider(self.counter);
+			
+	def saveSpider(self):
+		"""
+			No free lunch when it comes to saving and loading :(
+			
+			self.G=nx.DiGraph();
+			self.seen = Set();
+			self.counter = Counter();
+		
+		"""
+		print("saving spider...");
+		nx.write_gpickle(self.G, 'graph.gpickle');
+		with open('seen.dill', 'wb') as file:
+			dill.dump(self.seen, file);
+		with open('counter.dill', 'wb') as file:
+			dill.dump(self.counter, file);
+				
+	def loadSpider(self):
+		print("loading spider...");
+		self.G = nx.read_gpickle('graph.gpickle');
+		with open('seen.dill', 'rb') as file:
+			self.seen = dill.load(file);
+		with open('counter.dill', 'rb') as file:
+			self.counter = dill.load(file);
 			
 class CompositeHandler():
 	"""Handler to execute a list of handlers"""
@@ -116,17 +199,16 @@ class BasicAccount():
 			#		returnValues.apend(i['id']);
 		#return returnValues
 		
-	def getKeywordHighFrequency(self, query):
+	def getKeywordSearchCursor(self, query):
 		return tweepy.Cursor(self.api.search, q=query).items();
 		
 	def followHighFrequencySearch(self, query, freq, count):
-		return self.followHighFrequency(self.getKeywordHighFrequency(query), freq, count);
-			
-	def followHighFrequency(self, CURSOR, freq, count):
-		"""
-			Run through the cursor `count` times and follow anyone mentioned
-			more than `freq` times
-		"""
+		return self.followHighFrequency(self.getKeywordSearchCursor(query), freq, count);
+		
+	def getUserFrequencySearch(self, query, count):
+		return self.getFrequentUsers(self.getKeywordSearchCursor(query), count);
+		
+	def getFrequentUsers(self, CURSOR, count):
 		c = Counter();
 		l = [];
 		for i in CURSOR:
@@ -153,6 +235,14 @@ class BasicAccount():
 			else:
 				break;
 		c.update(l);
+		return c;
+			
+	def followFrequentUsers(self, CURSOR, freq, count):
+		"""
+			Run through the cursor `count` times and follow anyone mentioned
+			more than `freq` times
+		"""
+		c = getFrequencyCounts(self, CURSOR, count);
 		print c;
 		for key,val in c.most_common():
 			if(val > freq):
@@ -160,15 +250,37 @@ class BasicAccount():
 				API.create_friendship(key);
 	
 
-if __name__ == '__main__':
-
-	#This handles Twitter authetification and the connection to Twitter Streaming API
+def spiderBehavior():
 	auth = OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_token_secret)
-	API = tweepy.API(auth);
+	API = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True);
 	account = BasicAccount(API);
-	print account.followHighFrequencySearch(['numpy'], 5, 1000);
-
+	spider = FollowSpider(API);
+	try:
+		spider.loadSpider();
+		print("spider loaded successfully... resuming");
+		time.sleep(2);
+		try:
+			print("resuming spider");
+			spider.resumeSpider();
+		except KeyboardInterrupt:
+			print "interrupted!!!";
+			spider.saveSpider();
+	except IOError:
+		print("failed to load spider...");
+		print("creating new spider...");
+		time.sleep(2);
+		spider = FollowSpider(API);
+		initial = account.getUserFrequencySearch(['numpy'], 100);
+		try:
+			spider.startSpider(initial);
+		except KeyboardInterrupt:
+			print "interrupted!!!";
+			spider.saveSpider();
+			
+if __name__ == '__main__':
+	#This handles Twitter authetification and the connection to Twitter Streaming API
+	print("welcome to python ! haha ! programin is fun!!! lolz awesomesauce.jpg")
 	#l = StdOutListener(UsernameCounter(API))
 	#stream = Stream(auth, l)
 	#This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
